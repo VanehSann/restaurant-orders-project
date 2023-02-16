ac3de453ff3174e72126efc0baabb3c65fdb44eb
VanehSann <vanessacbsantos22@gmail.com>
2023-02-16 13:01:25 -0300

    fix: lint

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,12 +5,13 @@ import os
 def analyze_log(path):
     if not path.endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path}'\n")
-        
+
     if not os.path.isfile(path):
         raise FileNotFoundError(f"Arquivo inexistente: '{path}'\n")
 
     with open("data/mkt_campaign.txt", "w") as file:
-        file.write(f"INFO HERE\n")
+        file.write("INFO HERE\n")
+
 
 def read(path):
     with open(path) as csv_file:
--- src/track_orders.py
+++ src/track_orders.py
@@ -10,21 +10,26 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        self.orders.append({ "customer": customer, "order": order, "day": day })
+        self.orders.append({
+            "customer": customer,
+            "order": order,
+            "day": day
+              })
 
     def get_most_ordered_dish_per_customer(self, customer):
         list = []
         for order in self.orders:
-                list.append(order["order"])
+            list.append(order["order"])
         return max(Counter(list).most_common(), key=self.orders.count)[0]
 
     def get_never_ordered_per_customer(self, customer):
         list = []
         new = []
         for order in self.orders:
-                list.append(order["order"])
+            list.append(order["order"])
         for item in [day for day in list]:
-            if item != max(Counter(list).most_common(), key=self.orders.count)[0]:
+            info = Counter(list).most_common()
+            if item != max(info, key=self.orders.count)[0]:
                 new.append(item)
         return Counter(new).keys()
 
@@ -32,21 +37,20 @@ class TrackOrders:
         list = []
         new = []
         for order in self.orders:
-                list.append(order["day"])
+            list.append(order["day"])
         for item in [day for day in list]:
             if item != max(Counter(list), key=self.orders.count):
                 new.append(item)
         return Counter(new).keys()
-                
+
     def get_busiest_day(self):
         list = []
         for order in self.orders:
-                list.append(order["day"])
+            list.append(order["day"])
         return [result for result in Counter(list).keys()][0]
-
 
     def get_least_busy_day(self):
         list = []
         for order in self.orders:
-                list.append(order["day"])
+            list.append(order["day"])
         return [result for result in Counter(list).keys()][-1]
